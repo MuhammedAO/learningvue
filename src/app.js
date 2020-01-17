45f5e2a598caa6d509c7249ba503620d08937e71
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-01-17 23:26:33 +0100

    making HTTP requests with axios and jsonPH

--- src/app.js
+++ src/app.js
@@ -1,20 +1,31 @@
 import Vue from 'vue'
+window.axios = require ('axios')
 // import Accordion from './components/Accordion'
-import List from './components/List'
+// import List from './components/List'
 
 new Vue ({
 el: '#app',
 
 components: {
 // Accordion,
-List,
+// List,
+
+},
+
+mounted: async function  () {
+ let res = await axios.get('https://jsonplaceholder.typicode.com/posts')
+//   .then(res => console.log(res))
+// console.log(res);
+this.posts = res.data
+
 },
 
 data: {
-items: [
+    posts: null,
+// items: [
 // {title : 'Title 1', desc:'Description for title 1'},
 // {title : 'Title 2', desc:'Description for title 2'},
 // {title : 'Title 3', desc:'Description for title 3'}
-]
+// ]
 }
 });
